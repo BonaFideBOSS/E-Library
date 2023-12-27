eadb934e67dc75fdb26809c743fcc8b234c3dce7
Amaan Al Mir <amaanalmir@gmail.com>
2023-12-28 02:39:08 +0300

    Add files via upload

---
+++ config_sharable.py
@@ -0,0 +1,28 @@
+# General information about the website
+WEBSITE_INFO = {
+    "name": "E-Library",
+    "logo": '<i class="fa-sharp fa-solid fa-book-open-cover"></i>',
+    "description": "Online Library Management System",
+    "web_address": "elibrary.almir.info",
+}
+
+# Application secret key
+SECRET_KEY = b"1234567890"
+
+# Database connection string
+MONGO_URI = "mongodb+srv://Guest:C1L6dhI3Omklk97Y@cluster0.vizsb1w.mongodb.net/ELibraryDb"
+
+# Google SMPT (Mail) server credentials
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = 465
+MAIL_USERNAME = ""
+MAIL_PASSWORD = ""
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_DEFAULT_SENDER = ("E-Library", "contact@almir.info")
+
+# ImgBB API key
+IMGBB_API = ""
+
+# Filestack API key
+FILESTACK_API = ""
